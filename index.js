bb9b3b93dde6c03d927f953f19f3b8513089edf4
unknown <stam296@gmail.com>
2023-06-01 22:17:00 -0500

    including inquirer and fs packages. Made an inquirer prompt to ask user for the README title. Started writeToFile function to add the title to the README file.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title for your README?',
+            name: 'title',
+        },
+    ])
+    .then((response) =>
+        writeToFile('README.md', response)
+    );
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, `# ${data.title}`, (err) =>
+    err ? console.error(err) : console.log('README Generated')
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
